d555ae9ed39a148af7672de9a3d62747f6e90eec
Fordyta Abubakar <dhytodev@gmail.com>
2024-01-29 08:34:16 +0800

    feat: add date time format extension

---
+++ lib/src/core/date_time_ext.dart
@@ -0,0 +1,27 @@
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+class InvalidDateFormatException implements Exception {}
+
+extension DateTimeExt on String {
+  String toDateFormat({
+    String dateFormat = 'y-MM-dd HH:mm:ss',
+    bool toUtc = false,
+    bool toLocal = false,
+    String? locale = 'id',
+  }) {
+    if (toUtc && toLocal) throw InvalidDateFormatException();
+
+    initializeDateFormatting(locale, null);
+
+    final format = DateFormat(dateFormat, locale);
+
+    if (!toUtc && !toLocal) return format.format(toDateTime);
+
+    final date = toUtc ? toDateTime.toUtc() : toDateTime.toLocal();
+
+    return format.format(date);
+  }
+
+  DateTime get toDateTime => DateTime.parse(this);
+}
